6944d4dc4e6cbb34e5a2940990e497de50b3bb4f
Casey Kim <k.yesac@gmail.com>
2015-10-06 13:33:39 -0400

    fed hound

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.find_or_create_from_auth_hash(auth_hash)
-    User.find_or_create_by(provider: auth_hash.provider,
-      uid: auth_hash.uid) do |user|
+    User.find_or_create_by(
+      provider: auth_hash.provider,
+      uid: auth_hash.uid
+    ) do |user|
       user.name = auth_hash.info.name
       user.image = auth_hash.info.image
       user.email = auth_hash.info.email
