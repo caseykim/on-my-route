8794237c785dda5b5c25381c5845a08570669a65
Casey Kim <k.yesac@gmail.com>
2015-10-08 11:28:12 -0400

    fixed hound error

--- db/migrate/20151008021010_add_default_value_to_image.rb
+++ db/migrate/20151008021010_add_default_value_to_image.rb
@@ -2,6 +2,7 @@ class AddDefaultValueToImage < ActiveRecord::Migration
   def up
     change_column :users, :image, :string, default: '/thomastrain.jpg'
   end
+  
   def down
     change_column :users, :image, :string
   end
